e2912aa3e3b1ebe6bc0ae233511c2eb711cad637
GermanDiaz <germanrodrigo.2003@hotmail.com>
2024-11-11 01:16:25 -0500

    login page

---
+++ src/app/components/btn/btn.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-btn',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './btn.component.html'
+})
+export class BtnComponent {
+  
+  @Input() typeBtn: 'button' | 'reset' | 'submit' = 'button'
+
+}
